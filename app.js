e1af4603ee3be700e740b110a3e662eda6f961a0
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-20 11:33:34 +0000

    get-users endpoint created

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 require("./models/db");
 const createUserRouter = require("./routes/create-user");
+const getUserRouter = require("./routes/get-users");
 
 const app = express();
 
 app.use(express.json());
 
 app.use(createUserRouter);
+app.use(getUserRouter);
 
 const server = app.listen(7950, () => {
     console.log("Port is listening");
