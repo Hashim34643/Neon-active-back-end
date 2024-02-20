73f6bb94fe0c2a0558e2d2d8f52a24e5727f6390
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-20 12:10:17 +0000

    get-users testing implemented

--- controllers/get-users.js
+++ controllers/get-users.js
@@ -6,7 +6,6 @@ const getUsers = async (req, res) => {
 
         res.status(200).json({users});
     } catch (error) {
-        console.error("Error fetching users:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 };
---
+++ tests/get-users.test.js
@@ -0,0 +1,11 @@
+const app = require("../app");
+const request = require("supertest");
+
+describe("GET /users", () => {
+    test("Should respond with status 200 and return all users", async () => {
+        const response = await request(app).get("/users");
+        
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body.users)).toBe(true);
+    });
+});
