4bf76c21298d0a53a204278e33f81939d8f4cdc5
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-21 15:49:14 +0000

    /workouts endpoint implemented

---
+++ models/get-workouts.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const User = require("../models/create-user");
+
+const workoutSchema = new mongoose.Schema({
+    user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    type: {
+        type: String,
+        enum: ['cardio', 'weights', 'stretching'],
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    }
+});
+
+workoutSchema.statics.getAllWorkouts = async function() {
+    try {
+        const workouts = await this.find();
+        return workouts;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
