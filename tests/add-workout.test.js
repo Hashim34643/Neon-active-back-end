49b64bd8c3f7817c66c11be6e819ee6e6d9d1bac
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-21 14:02:16 +0000

    add-workout testing implemented

---
+++ tests/add-workout.test.js
@@ -0,0 +1,117 @@
+const request = require("supertest");
+const app = require("../app");
+const mongoose = require('mongoose');
+const Workout = require('../models/add-workout');
+
+const User = require("../models/create-user");
+
+let jwtToken = "";
+
+const createTestUser = async () => {
+    const userData = {
+        firstName: "Test",
+        lastName: "User",
+        email: "test@example.com",
+        password: "password"
+    };
+
+    const user = new User(userData);
+    await user.save();
+};
+
+const loginTestUser = async () => {
+    const userData = {
+        email: "test@example.com",
+        password: "password"
+    };
+
+    const response = await request(app)
+        .post("/login")
+        .send(userData);
+
+    jwtToken = response.body.token;
+    console.log(jwtToken)
+};
+
+beforeAll(async () => {
+    await mongoose.connection.dropDatabase();
+    await createTestUser();
+    await loginTestUser();
+});
+
+describe("POST /workouts/add", () => {
+    test("Should add a new workout", async () => {
+        const newUser = {
+            firstName: "TestFirstName",
+            lastName: "TestLastName",
+            email: "TestEmail@gmail.com".toLowerCase(),
+            password: "TestPassword",
+            confirmPassword: "TestPassword"
+        };
+        
+        const workoutData = {
+            type: "weights",
+            duration: 60
+        };
+
+        const response = await request(app)
+            .post("/workouts/add")
+            .set("Authorization", `jwt ${jwtToken}`)
+            .send(workoutData);
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty("newWorkout");
+        expect(response.body.newWorkout).toHaveProperty("_id");
+        expect(response.body.newWorkout).toHaveProperty("type", workoutData.type);
+        expect(response.body.newWorkout).toHaveProperty("duration", workoutData.duration);
+    });
+
+    test("Should respond with 401 if no JWT token is provided", async () => {
+        const workoutData = {
+            type: "weights",
+            duration: 60
+        };
+
+        const response = await request(app)
+            .post("/workouts/add")
+            .send(workoutData);
+
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("message", "Unauthorized access!");
+    });
+
+    test("Should respond with 401 if invalid JWT token is provided", async () => {
+        const workoutData = {
+            type: "weights",
+            duration: 60
+        };
+
+        const invalidToken = "INVALID_TOKEN";
+
+        const response = await request(app)
+            .post("/workouts/add")
+            .set("Authorization", `Bearer ${invalidToken}`)
+            .send(workoutData);
+
+        expect(response.statusCode).toBe(401);
+        expect(response.body).toHaveProperty("message", "Unauthorized access!");
+    });
+
+    test("Should respond with 400 if an invalid workout type is provided", async () => {
+        const workoutData = {
+            type: "invalid_type",
+            duration: 60
+        };
+
+        const response = await request(app)
+            .post("/workouts/add")
+            .set("Authorization", `Bearer ${jwtToken}`)
+            .send(workoutData);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty("message", "Invalid workout type");
+    });
+    afterAll(async () => {
+        await mongoose.connection.close();
+    });
+});
--- tests/get-user-byId.test.js
+++ tests/get-user-byId.test.js
@@ -1,24 +1,39 @@
 const User = require('../models/create-user')
 const app = require("../app");
 const request = require("supertest");
+const mongoose = require("mongoose")
 
 describe("GET /users/:id", () => {
-    test("Should respond with status 200", async () => {
-        let user_id = await User.findOne()
-        user_id = user_id.id.toString()
-        const response = await request(app).get(`/users/${user_id}`);
-        expect(response.statusCode).toBe(200);
-        
+    beforeAll(async () => {
+        const mongoURI = process.env.TEST_MONGO_URI;
+        await mongoose.connect(mongoURI);
+        await mongoose.connection.dropDatabase();
     });
     test("Should respond a user object with all properties", async () => {
+        const newUser = {
+            firstName: "TestFirstName",
+            lastName: "TestLastName",
+            email: "TestEmail@gmail.com".toLowerCase(),
+            password: "TestPassword",
+            confirmPassword: "TestPassword"
+        };
+        const user = await request(app).post("/create-user").send(newUser)
+        
         let user_id = await User.findOne()
         user_id = user_id.id.toString()
         const response = await request(app).get(`/users/${user_id}`);
         expect(response.body.users).toMatchObject({
             firstName: 'TestFirstName',
             lastName: 'TestLastName',
-            email: 'william123@gmail.com',
+            email: 'testemail@gmail.com',
         })
+        
+    });
+    test("Should respond with status 200", async () => {
+        let user_id = await User.findOne()
+        user_id = user_id.id.toString()
+        const response = await request(app).get(`/users/${user_id}`);
+        expect(response.statusCode).toBe(200);
         
     });
     test('400 status: should handle invalid user ID', async () => {
@@ -33,5 +48,8 @@ describe("GET /users/:id", () => {
     const response = await request(app).get(`/users/${nonExistentUserId}`);
     expect(response.statusCode).toBe(404); 
     expect(response.body.message).toBe('Invalid Id')
+    });
+    afterAll(async () => {
+        await mongoose.connection.close();
     });
 });
