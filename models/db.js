91d604ebaa2193c2c2eaa04e7fe735bc2c3b6f86
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-23 10:09:48 +0000

    deployed backend

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const ENV = process.env.NODE_ENV || 'test';
 const mongoURI = process.env.NODE_ENV === "test" ? process.env.TEST_MONGO_URI : process.env.MONGO_URI;
-if (!process.env.MONGO_URI && !process.env.TEST_MONGO_URI) {
+if (!process.env.MONGO_URI || !process.env.TEST_MONGO_URI || !process.env.DATABASE_URL) {
     throw new Error('Database or Test Database URL not set');
   }
 
