0e568cf6c61e698d4bad23f046d8181233073dd1
Hashim34643 <hashimhussain7312@gmail.com>
2024-02-28 09:54:41 +0000

    .

--- controllers/generate-workout-plan.js
+++ controllers/generate-workout-plan.js
@@ -3,12 +3,18 @@ const generateWorkoutPlan = require('../models/generate-workout-plan');
 const generateWorkoutPlanController = async (req, res) => {
   try {
     const { input1, input2, input3} = req.body;
-    const weeks = parseInt(input2, 10)
     if (!input1 || !input2 || !input3) {
       return res.status(400).json({ success: false, message: "Please provide all inputs" });
     }
 
-    const prompt = `generate a 1 week workout plan to ${input1}, i can exercise for ${input2} mins per day ${input3} times a week. give me the workout plan in bulletpoint format`;
+    const prompt = `generate a workout plan to ${input1}, i can exercise for ${input2} mins per day ${input3} times a week. give me the workout plan in this format:
+    Day 1:
+    Day 2:
+    Day 3:
+    Day 4:
+    Day 5:
+    Day 6:
+    Day 7:`;
     const workoutPlan = await generateWorkoutPlan(prompt);
     res.json({ success: true, workoutPlan });
   } catch (error) {
