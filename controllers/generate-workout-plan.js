8945ad5d8358940cdfd7fad64294561d8d1405f2
sw212 <94310203+sw212@users.noreply.github.com>
2024-02-29 10:13:24 +0000

    update generate workout plan prompt

--- controllers/generate-workout-plan.js
+++ controllers/generate-workout-plan.js
@@ -7,15 +7,25 @@ const generateWorkoutPlanController = async (req, res) => {
       return res.status(400).json({ success: false, message: "Please provide all inputs" });
     }
 
-    const prompt = `generate a workout plan to ${input1}, i can exercise for ${input2} mins per day ${input3} times a week. give me the workout plan in this format:
-    Day 1:
-    Day 2:
-    Day 3:
-    Day 4:
-    Day 5:
-    Day 6:
-    Day 7:
-    on the days that have a workout specify the warm-up, main workout and cooldown`;
+    const prompt = `generate a weekly workout plan to ${input1}, i can exercise for ${input2} mins per day ${input3} times a week. Your entire response must only be in the following format:
+    "
+    Day: *[1-7]*
+    warmup:
+    *warmup exercise description*
+    main exercise:
+    *first main exercise description*
+    *second main exercise description*
+    *third main exercise description*
+    cooldown:
+    *cooldown exercise description*
+    "
+    On rest days return:
+    "
+    Day: *[1-7]*
+    Rest Day
+    "
+    Do not respond with any additional information that does not follow the format provided in quotes. Only replace any parts of the provided format that lies between two '*', *[1-7]* means you must provide a number from 1 to 7 corresponding to the day of the workout for that week. 
+    `;
     const workoutPlan = await generateWorkoutPlan(prompt);
     res.json({ success: true, workoutPlan });
   } catch (error) {
